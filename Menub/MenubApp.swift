d305104c7d486f4f096846ff9fa0a49281b985e8
YangSeokHoR <aodde951@naver.com>
2026-07-07 13:12:36 +0900

    Initial Commit

---
+++ Menub/MenubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenubApp.swift
+//  Menub
+//
+//  Created by seokho on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MenubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenubTests/MenubTests.swift
@@ -0,0 +1,19 @@
+//
+//  MenubTests.swift
+//  MenubTests
+//
+//  Created by seokho on 7/7/26.
+//
+
+import Testing
+@testable import Menub
+
+struct MenubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
